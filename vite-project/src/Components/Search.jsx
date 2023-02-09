543fc7bf2c367ca167a65ca448ef1c437901e493
Treyce Wheeler <t.zeryon@gmail.com>
2023-02-09 14:23:41 -0500

    API work

--- vite-project/src/Components/Search.jsx
+++ vite-project/src/Components/Search.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import SearchResults from "./SearchResults";
 
+let gamesArray;
+
 export default function Search() {
   const [gameSearch, setGameSearch] = useState("");
   let inputField = "";
@@ -10,18 +12,24 @@ export default function Search() {
   searchRef.current = gameSearch;
 
   async function getGameData(searchText) {
-    let response = await fetch(
-      `https://api.rawg.io/api/games?key=3649c080caa8421eb6efd3638ba5b10e&search=${searchText}&ordering=-metacritic`
-    );
-    let data = await response.json();
-    return await data.results;
+    try {
+      let response = await fetch(
+        `https://api.rawg.io/api/games?key=3649c080caa8421eb6efd3638ba5b10e&search=${searchText}&ordering=-metacritic`
+      );
+      let data = await response.json();
+      return data.results;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      let data = getGameData(searchRef.current);
+      getGameData(gameSearch).then((games) => {
+        gamesArray = games;
+        console.log(gamesArray);
+      });
     }, 500);
-
     return function () {
       clearTimeout(timeout);
     };
--- vite-project/src/Components/SearchResults.jsx
+++ vite-project/src/Components/SearchResults.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function SearchResults(props) {
   let searchContainer = document.querySelector(".search-results");
   //   console.log(searchContainer);
-  if (props.text !== "") {
+  if (props.text !== "" && searchContainer !== null) {
     searchContainer.style.display = "flex";
+  } else if (props.text === "" && searchContainer !== null) {
+    searchContainer.style.display = "none";
   }
   return <div className="search-results">SearchResults</div>;
 }
